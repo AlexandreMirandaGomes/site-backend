d08448ab7c6835697a3c5441f3cc4599218403b2
colaborador <alexandre.m.gomes@grupoboticario.com.br>
2022-09-03 16:40:54 -0300

    Lombok

--- src/main/java/br/com/xande/sitebackend/Message.java
+++ src/main/java/br/com/xande/sitebackend/Message.java
@@ -1,8 +1,11 @@
 package br.com.xande.sitebackend;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
 @Entity
 public class Message {
     @Id
@@ -12,28 +15,7 @@ public class Message {
     @Column(length = 255)
     private String text;
     private LocalDateTime date;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
+
+
+
